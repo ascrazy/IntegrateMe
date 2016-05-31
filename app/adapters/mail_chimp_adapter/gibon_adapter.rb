b1a24fd6b7836bfd6ac8ea3e97fe8b3718bc1b0d
Alexandr Shahorsky <alexandr@ascrazy.me>
2016-05-31 16:16:55 +0000

    Improve error handling in GibonAdapter

--- app/adapters/mail_chimp_adapter/gibon_adapter.rb
+++ app/adapters/mail_chimp_adapter/gibon_adapter.rb
@@ -1,19 +1,29 @@
 module MailChimpAdapter
   class GibonAdapter
+    include ActiveSupport::Rescuable
+
+    rescue_from Gibbon::MailChimpError do |err|
+      raise MailChimpAdapter::MailChimpError, "#{err.title}: #{err.detail}"
+    end
+
+    rescue_from Gibbon::GibbonError do |err|
+      raise MailChimpAdapter::MailChimpError, err.message
+    end
+
     def initialize(api_key:, debug: false)
       @gibbon = Gibbon::Request.new(api_key: api_key, debug: debug)
     end
 
     def all_lists
       gibbon.lists.retrieve
-    rescue Gibbon::MailChimpError => err
-      raise MailChimpAdapter::MailChimpError, "#{err.title}: #{err.detail}"
+    rescue => err
+      rescue_with_handler(err) || raise
     end
 
     def find_list(list_id)
       response = gibbon.lists(list_id).retrieve
-    rescue Gibbon::MailChimpError => err
-      raise MailChimpAdapter::MailChimpError, "#{err.title}: #{err.detail}"
+    rescue => err
+      rescue_with_handler(err) || raise
     end
 
     def add_member_to_list(list_id, email:)
@@ -23,8 +33,8 @@ module MailChimpAdapter
           status: 'subscribed'
         }
       )
-    rescue Gibbon::MailChimpError => err
-      raise MailChimpAdapter::MailChimpError, "#{err.title}: #{err.detail}"
+    rescue => err
+      rescue_with_handler(err) || raise
     end
 
     private
